c9d30ef66f160c5af3129268702ab0205698da64
925sillerusa <rroyaltrove@gmail.com>
2025-09-28 04:44:06 -0400

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ maxWidth: 960, margin: "40px auto", padding: "0 16px" }}>
+      <h1 style={{ fontSize: 32, marginBottom: 8 }}>Welcome to 925 Siller</h1>
+      <p>Your site is deploying with Next.js. We’ll add products & checkout next.</p>
+    </main>
+  );
+}
